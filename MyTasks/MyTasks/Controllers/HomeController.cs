724e8d94e8bfea9260366931dbbdbe8ab005cd8c
Hung08 <hung.187pm20486@vanlanguni.vn>
2021-10-01 06:15:12 +0700

    Add Task

--- MyTasks/MyTasks/Controllers/HomeController.cs
+++ MyTasks/MyTasks/Controllers/HomeController.cs
@@ -38,5 +38,29 @@ namespace MyTasks.Controllers
                 return View();
             }
         }
+
+        [LoginVerification]
+        public ActionResult GetAllTasks()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            int id = (int)Session["uid"];
+            var tasks = db.Tasks.Where(x => x.IsDelete == false && x.UserId == id).ToList();
+            return Json(tasks, JsonRequestBehavior.AllowGet);
+        }
+
+        [LoginVerification]
+        [HttpPost]
+        public ActionResult AddTask(Task t)
+        {
+            int id = (int)Session["uid"];
+            Task task = new Task();
+            task.Description = t.Description;
+            task.IsDelete = false;
+            task.IsDone = false;
+            task.UserId = id;
+            db.Tasks.Add(task);
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ MyTasks/MyTasks/Scripts/app.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="angular.min.js" />
+var app = angular.module('ma', []);
+app.controller('mc', function ($scope, $http) {
+    $scope.GetAllTasks = function () {
+        $http({
+            method: "get",
+            url: "/Home/GetAllTasks"
+        }).then(function (response) {
+            $scope.tasks = response.data;
+        }, function () {
+            alert("Lỗi !!!");
+        });
+    }
+
+    $scope.AddTask = function () {
+        $scope.t = new Object();
+        $scope.t.Description = $scope.desc;
+        $http({
+            method: "post",
+            url: "/Home/AddTask",
+            datatype: "json",
+            data: JSON.stringify($scope.t)
+        }).then(function (response) {
+            $scope.GetAllTasks();
+            $scope.desc = "";
+        }, function () {
+            alert("Lỗi !!!");
+        });
+    }
+});
