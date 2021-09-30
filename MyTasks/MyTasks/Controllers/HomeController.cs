e0922cd3e843f09e0a84c19cd8c7d8e44ff62fc9
Hung08 <hung.187pm20486@vanlanguni.vn>
2021-10-01 03:51:13 +0700

    Add Template for website

--- MyTasks/MyTasks/Controllers/HomeController.cs
+++ MyTasks/MyTasks/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace MyTasks.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
