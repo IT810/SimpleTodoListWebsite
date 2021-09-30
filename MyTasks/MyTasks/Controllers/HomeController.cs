b3d7d9a3661ac4db60649d5ba0b23ebcd749af35
Hung08 <hung.187pm20486@vanlanguni.vn>
2021-10-01 04:51:29 +0700

    Complete login

--- MyTasks/MyTasks/Controllers/HomeController.cs
+++ MyTasks/MyTasks/Controllers/HomeController.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyTasks.Models;
+using MyTasks.MiddleWare;
 
 namespace MyTasks.Controllers
 {
     public class HomeController : Controller
     {
+        private MyDBEntities db = new MyDBEntities();
+
+        [LoginVerification]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            var user = db.Users.SingleOrDefault(x => x.Email == email.Trim() && x.Password == password.Trim());
+            if(user != null) {
+                Session["uid"] = user.Id;
+                Session["uname"] = user.Name;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
---
+++ MyTasks/MyTasks/MiddleWare/LoginVerification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTasks.MiddleWare
+{
+    public class LoginVerification : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["uid"] == null)
+            {
+                filterContext.Result = new RedirectResult("~/Home/Login");
+                return;
+            }
+        }
+    }
+}
